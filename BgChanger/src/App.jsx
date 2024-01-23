0274f866e85049c31ca6167302a35bd7d3a4c42d
Sridhar1030 <77533416+Sridhar1030@users.noreply.github.com>
2024-01-23 10:33:48 +0530

    Finish BgChanger

--- BgChanger/src/App.jsx
+++ BgChanger/src/App.jsx
@@ -4,14 +4,33 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color,setcolor] = useState('olive')
+
+  // function changeColor(color){
+  //   setcolor(color)
+  // }
 
   return (
-    <div className='w-full h-screen duration-200 bg-black'>
+    <div className='w-full h-screen duration-200 text-2xl' style={{backgroundColor: color}}>
+      HELLO
       <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2' >
         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3
         py-2 rounded-3xl'>
-          <button>Test</button> 
+          <button 
+            onClick={() => setcolor('red')}
+          className='outline-none px-4 rounded-full text-black shadow-lg ' style={{color:color}}>Red</button> 
+          <button 
+          onClick={() => setcolor('green')}
+          className='outline-none px-4 rounded-full text-black shadow-lg 'style={{color:color}}>Green</button> 
+          <button 
+          onClick={() => setcolor('yellow')}
+          className='outline-none px-4 rounded-full text-black shadow-lg 'style={{color:color}}>Yellow</button> 
+          <button 
+          onClick={() => setcolor('blue')}
+          className='outline-none px-4 rounded-full text-black shadow-lg 'style={{color:color}}>Blue</button> 
+          <button 
+          onClick={() => setcolor('white')}
+          className='outline-none px-4 rounded-full text-black shadow-lg 'style={{color:color}}>White</button> 
         </div>  
       </div>
 
